e53227a6a5060fb3b488837c61c226d7119ea145
Rob <rob.knights@hotmail.com>
2018-02-04 19:41:55 +0000

    Seperated code. get_url.py contains the request method. get_app_ids.py scrapes multiple pages for application ids - currently writes to text file. app_scraper.py takes app ids and scrapes data from their pages (multithreaded).

---
+++ app_scraper.py
@@ -0,0 +1,78 @@
+from bs4 import BeautifulSoup
+from pprint import pprint
+from multiprocessing.pool import ThreadPool
+from time import time as timer
+import json
+from get_url import simple_get
+
+
+def get_app_details(app_id):
+    print("Getting details for app with id '{}'".format(app_id))
+    details = {}
+    app_page_url = "https://play.google.com/store/apps/details?id=" + app_id + "&hl=en_GB"
+    raw_html = simple_get(app_page_url)
+    if not raw_html:
+        return app_id, None
+    soup = BeautifulSoup(raw_html, 'html.parser')
+    title = soup.find("div", {"class": "id-app-title"}).text
+    details['title'] = title.strip()
+    genre = soup.find("span", {"itemprop": "genre"}).text
+    details['genre'] = genre.strip()
+    desc = soup.find("div", {"itemprop": "description"}).text
+    desc = desc.replace('<br>', '\n')
+    details['description'] = desc.strip()
+
+    meta_info = soup.findAll("div", {"class": "meta-info"})
+    for info in meta_info:
+        info_title = info.find("div", {"class": "title"}).text
+        info_content = info.find(attrs={"class": "content"}).text
+        details[info_title.strip()] = info_content.strip()
+
+    pprint(details)
+
+    return app_id, details
+
+
+def get_many_app_details(app_ids):
+    print("Getting app details for {}.".format(', '.join(app_ids)))
+    start = timer()
+    results = ThreadPool(20).imap_unordered(get_app_details, app_ids)
+    app_dict = {}
+    for result in results:
+        app_dict[result[0]] = result[1]
+
+    with open('apps.json') as app_json:
+        current_apps = json.load(app_json)
+
+    current_apps.update(app_dict)
+
+    with open('apps.json', 'w') as app_json:
+        json.dump(current_apps, app_json, indent=4)
+
+    print("Elapsed Time: %s" % (timer() - start,))
+
+
+def get_app_ids(max_count):
+    print("Retrieving a maximun of {} app_ids".format(max_count))
+    with open('apps.json') as apps_json:
+        json_content = json.load(apps_json)
+    with open('app_ids.txt') as ids_file:
+        app_ids = [app_id.strip() for app_id in ids_file if app_id.strip()]
+    new_ids = []
+    for app_id in app_ids:
+        if app_id not in json_content:
+            new_ids.append(app_id)
+            print("App id '{}' not in current json and has been returned.".format(app_id))
+        else:
+            if not json_content[app_id]:
+                new_ids.append(app_id)
+                print("App id '{}' contains no information in current json and has been returned.".format(app_id))
+        if len(new_ids) == max_count:
+            break
+    return new_ids
+
+
+if __name__ == "__main__":
+    # get multiple app info
+    app_ids = get_app_ids(15)
+    get_many_app_details(app_ids)
---
+++ get_app_ids.py
@@ -0,0 +1,78 @@
+from bs4 import BeautifulSoup
+from pprint import pprint
+from get_url import simple_get
+
+
+def get_app_categories(raw_html):
+    categories = []
+    html = BeautifulSoup(raw_html, 'html.parser')
+    for a in html.select('a'):
+        # Ignore 'Games' and 'Family' section of dropdown
+        if a['href'].startswith("/store/apps/category/") and \
+                not a['href'].startswith("/store/apps/category/GAME") and \
+                not a['href'].startswith("/store/apps/category/FAMILY"):
+            categories.append(a['href'])
+    return categories
+
+
+def get_sub_categorys(url):
+    sub_categories = {}
+    raw_html = simple_get(url)
+    html = BeautifulSoup(raw_html, 'html.parser')
+    data = html.findAll('h2')
+    for h2 in data:
+        for a in h2.select('a'):
+            name = a.text.strip()
+            url = a['href']
+            if name != "See more" and name != "Recommended for you":
+                sub_categories[name] = url
+    # pprint(sub_categories)
+    return sub_categories
+
+
+def get_apps_on_page(url):
+    app_ids = set()
+    raw_html = simple_get(url)
+    soup = BeautifulSoup(raw_html, 'html.parser')
+    for a in soup.select('a'):
+        signature = "/store/apps/details?id="
+        if a['href'].startswith(signature):
+            app_id = a['href'][len(signature):]
+            app_ids.add(app_id)
+    return app_ids
+
+
+if __name__ == "__main__":
+    main_page_url = "https://play.google.com"
+
+    # get main categories
+    app_store_page = main_page_url + "/store/apps"
+    raw_html = simple_get(app_store_page)
+    categories = get_app_categories(raw_html)
+    pprint(categories)
+
+    # get sub categories (test)
+    # url = "https://play.google.com/store/apps/category/COMMUNICATION"
+    # sub_cats = get_sub_categorys(url)
+
+    # get all app ids from page (test)
+    # url = "https://play.google.com/store/apps/collection/promotion_cp_messaging_apps?clp=SjIKIQobcHJvbW90aW9uX2NwX21lc3NhZ2luZ19hcHBzEAcYAxINQ09NTVVOSUNBVElPTg%3D%3D:S:ANO1ljIyLhU"
+    # print(get_apps_on_page(url))
+
+    # get app ids from multiple pages
+    all_app_ids = set()
+    for category in categories:
+        c_url = main_page_url + category
+        sub_categories = get_sub_categorys(c_url)
+        for name, sub_category_path in sub_categories.items():
+            print("Trying '{}' page at path '{}'".format(name, sub_category_path))
+            sc_url = main_page_url + sub_category_path
+            app_ids_on_page = get_apps_on_page(sc_url)
+            all_app_ids = all_app_ids.union(app_ids_on_page)
+
+    with open('app_ids.txt', 'w') as f:
+        for indx, app_id in enumerate(all_app_ids):
+            if indx == 0:
+                f.write(app_id)
+            else:
+                f.write("\n" + app_id)
